9f5de0f46b534b9928a65f28bab20e62e790924d
Fernando <fgonzalezaguilera@gmail.com>
2015-12-08 22:08:22 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='scrapazhon',
+      version='0.1',
+      description='Scrape Amazon App Store',
+      url='http://github.com/fegoa89',
+      author='Fernando González Aguilera',
+      author_email='fgonzalezaguilera@gmail.com',
+      license='MIT',
+      packages=['scrapazhon'],
+      zip_safe=False)
