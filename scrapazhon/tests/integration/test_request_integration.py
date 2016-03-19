179aba79000894cf9d4fafc92b31a498d0603d99
Fernando <fgonzalezaguilera@gmail.com>
2016-03-19 20:25:57 +0100

    moved response to setUp block

--- scrapazhon/tests/integration/test_request_integration.py
+++ scrapazhon/tests/integration/test_request_integration.py
@@ -18,6 +18,15 @@ from bs4 import BeautifulSoup
 
 class RequestTest(unittest.TestCase):
 
+    def setUp(self):
+        self.requestObject = Request("http://www.amazon.com/appstore").getHtmlFromUrl()
+        return self.requestObject
+
     def valid_response_code_test(self):
-        requestObject = Request("http://www.amazon.com/appstore")
-        self.assertEqual(requestObject.getHtmlFromUrl()['code'], 200)
+        self.assertEqual(self.requestObject['code'], 200)
+
+    def valid_parsed_response_test(self):
+        # Find amazon app store title on page
+        soup = BeautifulSoup(self.requestObject['response'], "lxml")
+
+        self.assertEquals(soup.find("div", {"class": "acs-bgh1-header"}).text, '\nAmazon Appstore\n')
