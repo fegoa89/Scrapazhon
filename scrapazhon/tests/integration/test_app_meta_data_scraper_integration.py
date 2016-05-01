af1e5524bbc07ce5db373364672b35899f910b02
Fernando <fgonzalezaguilera@gmail.com>
2016-05-01 13:43:46 +0200

    added more tests to test_app_meta_data_scraper_integration.py

--- scrapazhon/tests/integration/test_app_meta_data_scraper_integration.py
+++ scrapazhon/tests/integration/test_app_meta_data_scraper_integration.py
@@ -22,6 +22,8 @@ from mock               import patch
 
 from bs4                import BeautifulSoup
 
+from datetime           import datetime
+
 from scrapazhon.app_meta_data_scraper import AppMetaDataScraper
 
 class AppMetaDataScraperTest(unittest.TestCase):
@@ -59,4 +61,34 @@ class AppMetaDataScraperTest(unittest.TestCase):
         self.assertEqual(isinstance(self.response["keywords"], list), True)
 
         for keyword in self.response["keywords"]:
-            self.assertEqual(isinstance(keyword, basestring), True)        
+            self.assertEqual(isinstance(keyword, basestring), True)
+
+    def price_test(self):
+        self.assertEqual(isinstance(self.response["price"], basestring), True)
+
+    def app_publisher_name_test(self):
+        self.assertEqual(isinstance(self.response["app_publisher_name"], basestring), True)
+
+    def customer_reviews_count_test(self):
+        self.assertEqual(isinstance(self.response["customer_reviews_count"], int), True)
+
+    def average_customer_review_test(self):
+        self.assertEqual(isinstance(self.response["average_customer_review"], float), True)
+
+    def latest_update_date_test(self):
+        self.assertEqual(isinstance(self.response["latest_update_date"], datetime), True)
+
+    def release_date_test(self):
+        self.assertEqual(isinstance(self.response["release_date"], datetime), True)
+
+    def rated_on_category_test(self):
+        self.assertEqual(isinstance(self.response["rated_on_category"], basestring), True)
+
+    def product_features_test(self):
+        self.assertEqual(isinstance(self.response["product_features"], list), True)
+
+        for feature in self.response["product_features"]:
+            self.assertEqual(isinstance(feature, basestring), True)
+
+    def app_description_test(self):
+        self.assertEqual(isinstance(self.response["app_description"], basestring), True)
