a82f624508ba81eca94d8bf008946ee719b38b8b
Fernando <fgonzalezaguilera@gmail.com>
2016-05-22 14:08:56 +0200

    commented one test - deleted update_tests_fixtures.py

--- scrapazhon/tests/integration/test_request_integration.py
+++ scrapazhon/tests/integration/test_request_integration.py
@@ -29,8 +29,8 @@ class RequestTest(unittest.TestCase):
     def valid_response_code_test(self):
         self.assertEqual(self.requestObject['code'], 200)
 
-    def valid_parsed_response_test(self):
+    # def valid_parsed_response_test(self):
         # Find amazon app store title on page
-        soup = BeautifulSoup(self.requestObject['response'], "lxml")
-
-        self.assertEquals(soup.find("div", {"class": "acs-bgh1-header"}).text, '\nAmazon Appstore\n')
+        # Commented for now - does not contain "Amazon Appstore" text anymore on page
+        # soup = BeautifulSoup(self.requestObject['response'], "lxml")
+        # self.assertEquals(soup.find("div", {"class": "acs-bgh1-header"}).text, '\nAmazon Appstore\n')
