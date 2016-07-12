653c19ff8d5ecfd01406eac7ab43b74539b82014
Fernando <fgonzalezaguilera@gmail.com>
2016-07-12 22:28:03 +0200

    assign zg_title div response to a variable and used for app_id, app_url and app_title

--- scrapazhon/top_charts_scraper.py
+++ scrapazhon/top_charts_scraper.py
@@ -18,9 +18,11 @@ class TopChartsScraper:
         apps_in_page = self.soup_object().find("div", {"id":"zg_centerListWrapper"}).findAll("div", {"class":"zg_itemImmersion"})
         for app in apps_in_page:
             ranked_app = {}
+            div_zg_title_dom_element     = app.find("div", {"class": "zg_title"}).find('a')
             ranked_app["rank_position"]  = self.sanitize_rank_position(app.find("span", {"class": "zg_rankNumber"}).get_text())
-            ranked_app["app_title"]      = app.find("div", {"class": "zg_title"}).find('a').get_text()
-            ranked_app["app_url"]        = app.find("div", {"class": "zg_title"}).find('a')['href'].strip()
+            ranked_app["app_title"]      = div_zg_title_dom_element.get_text()
+            ranked_app["app_url"]        = div_zg_title_dom_element['href'].strip()
+            ranked_app["app_id"]         = div_zg_title_dom_element['href'].strip().split("/dp/")[1].split("/")[0]
             ranked_app["app_icon_small"] = app.find("div", {"class": "zg_itemImageImmersion"}).find('a').find('img')['src'].strip()
 
             top_paid_apps_array.append(ranked_app)
